3e1b550018daa5a09942c5b8c9a8048aaaf15221
ruby <rehab.khaled.bahnasy@gmail.com>
2024-05-05 21:38:15 +0300

    Fixed Comparable. Finished HuffmanNode. Finished frequency method.

--- ImageEncryptCompress/HuffmanNode.cs
+++ ImageEncryptCompress/HuffmanNode.cs
@@ -1,12 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing.Text;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms.VisualStyles;
 
-namespace ImageEncryptCompress
+public class HuffmanNode : IComparable<HuffmanNode>
 {
-    internal class HuffmanNode
+    public int frequency;
+    private HuffmanNode leftNode;
+    private HuffmanNode rightNode;
+    public HuffmanNode(HuffmanNode leftNode, HuffmanNode rightNode, int frequency)
     {
+        this.leftNode = leftNode;
+        this.rightNode = rightNode;
+        this.frequency = frequency;
+    }
+    public int CompareTo(HuffmanNode node)
+    {
+        return node.frequency - frequency;
     }
 }
+
--- ImageEncryptCompress/ImageUtilities.cs
+++ ImageEncryptCompress/ImageUtilities.cs
@@ -3,10 +3,54 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ImageEncryptCompress;
 
-namespace ImageEncryptCompress
+/// <summary>
+/// Calculates the frequency of each color value in a specified channel of an image.
+/// </summary>
+/// <param name="image"> A 2D array of RGBPixel objects representing the image.</param>
+/// <param name="channel"> An integer indicating the channel to calculate the frequency for:
+///   - 0: Blue channel
+///   - 1: Green channel
+///   - 2: Red channel
+/// </param>
+/// <returns>
+/// An integer array of size 255 where the index represents the color value (0-255)
+/// and the value at that index represents the frequency of that color value in the
+/// specified channel.
+/// </returns>
+public class ImageUtilities
 {
-    internal class ImageUtilities
+    public int[] GetColorChannelFrequency(RGBPixel[,] image, int channel)
     {
+        int[] colorFrequency = new int[256];
+        for (int i = 0; i < image.GetLength(0); i++)
+        {
+            for (int j = 0; j < image.GetLength(1); j++)
+            {
+                int colorValue = 0;
+                switch (channel)
+                {
+                    case 0:
+                        {
+                            colorValue = image[i, j].blue;
+                            break;
+                        }
+                    case 1:
+                        {
+                            colorValue = image[i, j].green;
+                            break;
+                        }
+                    case 2:
+                        {
+                            colorValue = image[i, j].red;
+                            break;
+                        }
+                }
+                colorFrequency[colorValue]++;
+            }
+        }
+        return colorFrequency;
     }
 }
+
